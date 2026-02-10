94f13c2084d7929c6494fd475fd63e10d4eacf5f
HugueAX <rvnchsn@gmail.com>
2026-02-09 23:29:39 -0500

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+import './App.css';
+
+function Header({name, year}) {
+  return(
+    <header>
+      <h1>{name}'s Kitchen</h1>
+      <p>Copyright {year}</p>
+    </header>
+  );
+}
+const items = [
+  "Macaroni and Cheese",
+  "Salmon with Potatoes",
+  "Tofu with Vegetables"
+];
+
+function Main({dishes}) {
+  return (
+  <ul>
+    {dishes.map((dish) => (
+      <li style={{listStyleType: "none"}}>{dish}</li>
+    ))}
+  </ul>
+  )}
+
+function App() {
+  return (
+  <div>
+    <Header name="Alex" year={2024}/>
+    <Main dishes={items}/>
+  </div>
+  );
+}
+
+export default App
